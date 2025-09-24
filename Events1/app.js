41e001d87489593a8d0ebd9c1d33333eb5a96789
Abhishek <abhikam152002@gmail.com>
2025-09-24 21:01:10 +0530

    Events tasks

---
+++ Events1/app.js
@@ -0,0 +1,19 @@
+let par = document.querySelector("p");
+par.style.color = "red";
+
+// For the next tasks;
+
+let newPar = document.querySelector("#newPara");
+let newBtn = document.querySelector("#newBtn");
+
+
+newBtn.onclick = function() {
+    if ( newPar.innerText = "Hello sir Student here"){
+        newPar.innerText = "  Will Graduate in 2026";
+    } else{
+        newPar.innerText = " Coder here ";
+    }
+}
+
+// Now changing the color of the text and improving it's size:- 
+
