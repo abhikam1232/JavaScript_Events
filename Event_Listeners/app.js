51c7a6ff887da9576e95faa2ca11c3ee13548485
Abhishek <abhikam152002@gmail.com>
2025-09-28 18:09:15 +0530

    EventListener

--- Event_Listeners/app.js
+++ Event_Listeners/app.js
@@ -4,3 +4,16 @@ btn.addEventListener( "click", function() {
     console.info(" This button has been clicked");
     alert( " You clicked it");
 } );
+
+
+// Now  double clicke using event listener:- 
+
+let myButton = document.querySelector("#myBtn"); 
+myButton.style.marginTop = "20px";
+
+myButton.addEventListener("dblclick", function() {
+    console.log(" This button has been doble clicked");
+    myButton.innerText = "Button";
+    myButton.style.color = "blue";
+    myButton.backgroundColor = " whitesmoke";
+});
