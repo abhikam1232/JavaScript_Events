e6540ef45b6796248660f9c7fed1e1d44c2d6266
Abhishek <abhikam152002@gmail.com>
2025-09-30 13:05:20 +0530

    Color generator

---
+++ Activity/app.js
@@ -0,0 +1,22 @@
+let btn = document.querySelector("button");
+
+btn.addEventListener( "click", function() {
+    let h3 = document.querySelector("h2");
+    let randomColor = getrandomColor();
+    h3.innerText = randomColor;
+
+    console.log("Color Updated");
+
+    let div = document.querySelector("div");
+    div.style.backgroundColor = randomColor;
+
+});
+
+function getrandomColor () {
+    let red = Math.floor(Math.random() * 255);
+    let green = Math.floor(Math.random() * 255);
+    let blue = Math.floor(Math.random() * 255);
+
+    let color = `rgb (${red}, ${green}, ${blue})`;
+    return color;
+}
