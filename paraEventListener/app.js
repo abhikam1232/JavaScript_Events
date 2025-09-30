fcd0883e727fc94aa67b8bee24c0858af4ef2c40
Abhishek <abhikam152002@gmail.com>
2025-09-30 21:20:00 +0530

    Revise

--- paraEventListener/app.js
+++ paraEventListener/app.js
@@ -2,4 +2,12 @@ let par = document.querySelector("p");
 
 par.addEventListener( "click", function() {
     console.info(" This Para is basically Clicked");
+});
+
+let box = document.querySelector(".box");
+
+box.addEventListener("mouseenter", function() {
+    for ( let i=0; i<=5; i++){
+        console.info(" Cursor has been on the Mouse");
+    }
 })
