19d809012ec72b041947a9009678d2cbeb1ce2fa
Abhishek <abhikam152002@gmail.com>
2025-10-02 17:21:55 +0530

    updated

--- List-Maker/app.js
+++ List-Maker/app.js
@@ -2,46 +2,35 @@ let input = document.querySelector("#input");
 input.style.marginTop = "20px";
 input.style.fontSize = "1.1em";
 
-let btnAdd = document.querySelector("#add"); // Add Button Section 
+let btnAdd = document.querySelector("#add");
 btnAdd.style.paddingLeft = "5px";
 btnAdd.style.paddingRight = "5px";
 
-let btnRemove = document.querySelector("#Remove"); // Remove Button Section 
+let btnRemove = document.querySelector("#Remove");
 btnRemove.style.paddingLeft = "5px";
 btnRemove.style.paddingRight = "5px";
 
-let div = document.querySelector("div");
+// Create ul and append to body or a specific container
+let ul = document.createElement("ul");
+document.body.appendChild(ul); // Append to body instead of div
 
-let ul = document.createElement("ul"); // New ul created 
-div.appendChild(ul);
-
-
-
-// Adding tasks inside the box:-
+// Adding tasks
 btnAdd.addEventListener("click", function() {
     let task = input.value.trim();
-
-    if ( task !== ""){
-        let li = document.createElement("li");
     
-
-    li.textContent = task;
-
-    ul.appendChild(li);
-
-    input.value = "";
+    if (task !== "") {
+        let li = document.createElement("li");
+        li.textContent = task;
+        ul.appendChild(li);
+        input.value = "";
     }
 });
 
-
-
+// Removing tasks
 btnRemove.addEventListener("click", function() {
-    // Select the last list item in the <ul>
     let lastTask = ul.lastElementChild;
-
-    // Check if there is a last task to remove
+    
     if (lastTask) {
-        // Remove that task from the <ul>
         ul.removeChild(lastTask);
     }
 });
