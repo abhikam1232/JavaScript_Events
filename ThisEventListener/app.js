552e437aefca8aa2688f16a409b8bcd909d5657a
Abhishek <abhikam152002@gmail.com>
2025-10-02 20:59:03 +0530

    this in Events

--- ThisEventListener/app.js
+++ ThisEventListener/app.js
@@ -9,7 +9,7 @@ button.addEventListener( "click", function() {
 
 });
 
-let h3 = document.querySelector("h3");
+/** let h3 = document.querySelector("h3");
 h3.addEventListener("click", function() { // This is the old method 
     h3.style.backgroundColor = "aqua";
 });
@@ -20,5 +20,29 @@ function changeColor() {
 };
 
 let h1 = document.querySelector("h1");
-h1.addEventListener("click", changeColor);
+h1.addEventListener("click", changeColor); **/
+
+// Now this is the code again for this EventListener:-
+
+let h1 = document.querySelector("h1"); // In this section we selcted these elements 
+let h2 = document.querySelector("h2");
+let h3 = document.querySelector("h3");
+let par = document.querySelector("p");
+
+function darkMode() {
+    this.style.backgroundColor = "black";
+    this.style.color = "azure";
+};
+
+h1.addEventListener("click", darkMode);
+
+h2.addEventListener("click", darkMode);
+
+h3.addEventListener("click", darkMode);
+
+par.addEventListener("click", darkMode);
+
+
+
+
 
