f425a7926e7cb8783d89e842c052af3325014b8a
Abhishek <abhikam152002@gmail.com>
2025-10-03 15:46:48 +0530

    this EventListener Activity

--- ThisEventListener/app.js
+++ ThisEventListener/app.js
@@ -71,7 +71,54 @@ par.addEventListener("mouseenter", hoverMe);
 par.addEventListener("mouseleave", noHover);
 
 
+//Create a function that, when you click on any element (h2, h3, p, and button), it changes the inner text of that element to say "You clicked me!", but when you double-click it, it should reset back to the original text.
 
+let two = document.querySelector(".headerTwo"); // Accessed all elemnts for performings task:- 
+let three = document.querySelector(".headerThree");
+let para = document.querySelector(".paragraph");
+let btn = document.querySelector("#unique");
 
+function clickmeNow () {
+    this.innerText = "You Clicked ME $$$ "; // This is comman for all
+};
 
+function reverseClick() {
+    let a = "Asus Vivobook Click"; // This is for header two 
+    this.innerText = a;
+};
 
+function reverseClickTwo () {
+    let b = "Events in JavaScript performing tasks"; // This is for header three
+    this.innerText = b;
+};
+
+function reverseClick3 () {
+    let c = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa blanditiis optio exercitationem, officiis et neque obcaecati eum excepturi dolorum adipisci voluptatibus, voluptatem dolores modi porro commodi tenetur quos quae deleniti?";
+    this.innerText = c;    // This is for paragraph 
+};
+
+function reverseBtn() {
+    this.innerText = "Touch me now";
+    this.style.color = "green";
+    this.style.backgroundColor = "AliceBlue";
+    this.style.fontSize = "1.3em";
+    this.style.borderWidth = "2px";
+    this.style.borderStyle = "solid";
+    this.style.borderColor = "maroon";
+
+    setTimeout( function() {
+        removeEventListener.btn();
+    }, 2000);
+}
+
+two.addEventListener("click", clickmeNow);
+two.addEventListener("dblclick", reverseClick);
+
+three.addEventListener("click", clickmeNow);
+three.addEventListener("dblclick", reverseClickTwo);
+
+para.addEventListener("click", clickmeNow);
+para.addEventListener("dblclick", reverseClick3);
+
+btn.addEventListener("click", clickmeNow);
+btn.addEventListener("click", reverseBtn);
