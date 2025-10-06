dc43b500d21e3eab3f093f8fae5739aac99646c0
Abhishek <abhikam152002@gmail.com>
2025-10-06 14:33:33 +0530

    keydown event

--- keyboardEvents/app.js
+++ keyboardEvents/app.js
@@ -8,4 +8,12 @@ btn.addEventListener("click", function(event) {
 btn.addEventListener("dblclick", function(event) {
     console.log(event);
     console.log("Button has been Clicked !!");
+});
+
+// Now accessing the input button:- 
+let input = document.querySelector("input");
+input.style.margin = " 10px";
+
+input.addEventListener("keydown", function() {  // This is accessed if anyKey has pressed !! 
+    console.log(" You have pressed a Key !!");
 })
