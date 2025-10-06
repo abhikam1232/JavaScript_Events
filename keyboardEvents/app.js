34d898d38f80ed3a9a5e45a63b14e4b8bd0707c9
Abhishek <abhikam152002@gmail.com>
2025-10-06 14:45:08 +0530

    keyup

--- keyboardEvents/app.js
+++ keyboardEvents/app.js
@@ -14,6 +14,14 @@ btn.addEventListener("dblclick", function(event) {
 let input = document.querySelector("input");
 input.style.margin = " 10px";
 
-input.addEventListener("keydown", function() {  // This is accessed if anyKey has pressed !! 
+input.addEventListener("keydown", function(event) {  // This is accessed if anyKey has pressed !! 
+    console.log(event);
     console.log(" You have pressed a Key !!");
 })
+
+// Now this is for keyup event:- 
+
+input.addEventListener("keyup", function(event) {
+    console.log(event);
+    console.log(" Now you have released a key");
+});
