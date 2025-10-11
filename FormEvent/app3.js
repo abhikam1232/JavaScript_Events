03e646d2f16ee3200fa6a86456ef03380ff3852c
Abhishek <abhikam152002@gmail.com>
2025-10-11 16:03:11 +0530

    details updated in the form

--- FormEvent/app3.js
+++ FormEvent/app3.js
@@ -1,11 +1,15 @@
 // Now accessing all the elements from the form:- 
-let name = document.querySelector("#name");
+let user = document.querySelector("#name");
 let mail = document.querySelector("#mail");
 let age = document.querySelector("#num");
 let span = document.querySelector("span");
 let btn = document.querySelector("button");
+let form = document.querySelector("form");
+let gender = document.querySelector("#gender");
 btn.style.marginTop = "10px";
 
+
+// This Section was for the span tag:- 
 span.addEventListener("mouseenter", function(){
     span.style.backgroundColor = "#555555";
     span.style.color = "azure";
@@ -17,6 +21,23 @@ span.addEventListener("mouseenter", function(){
     span.style.paddingLeft = "5px";
     span.style.border = "none";
 });
+
+form.addEventListener("submit", function(eve) {
+    eve.preventDefault();
+    console.info(`This is my registered name:- ${user.value}`);
+    console.info(`This mail id you have Registered ${mail.value}`);
+    console.info(`Password is hidden due to legal Restrictions :- ****`);
+    
+    /**if (gender.value == "male"){
+        console.info("male");
+    } else{
+        console.info("female");
+    } **/ // In contruction.. 
+
+
+} );
+
+
 
 
 
