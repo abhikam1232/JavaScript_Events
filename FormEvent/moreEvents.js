75028ac2cf687b8e27e95d0cb85595eae0a9fc79
Abhishek <abhikam152002@gmail.com>
2025-10-12 17:09:34 +0530

    live name monitor

--- FormEvent/moreEvents.js
+++ FormEvent/moreEvents.js
@@ -19,3 +19,21 @@ myBox.addEventListener("input", function() {
     console.log(" This is input event");
     console.log(" final value = ", this.value);
 })
+
+// Task:- Live name display with final confirmation..
+
+let input = document.querySelector("#inp");
+let bttn = document.querySelector(".btn");
+
+
+// This is used to check the live name of the user...
+input.addEventListener("input", function(){
+    console.log(" This will check each alpahbet as we type the name:- ");
+    console.log("Final input ", this.value);
+});
+
+// And this is used to check whether the user has entered his name at the final using change...
+input.addEventListener("change", function() {
+    console.log("change event for name checking");
+    console.log("Your complete name:- ", this.value);
+})
