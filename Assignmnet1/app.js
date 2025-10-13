c8a9c50d72c48138875ccd45dad6452d283a6f74
Abhishek <abhikam152002@gmail.com>
2025-10-13 15:02:38 +0530

    keypress

--- Assignmnet1/app.js
+++ Assignmnet1/app.js
@@ -5,12 +5,24 @@ container.addEventListener("mouseout", function(eve) {
     eve.preventDefault();
     container.style.backgroundColor = "#708A58";
     container.style.color = "#ecf3e6ff";
-    text.style.innerText = " Mouseout Applied";
+    text.innerText = " Mouseout Applied";
     container.style.borderRadius = "18px";
 });
 
 container.addEventListener("mouseover", function() {
     container.style.color = "#000000";
     container.style.backgroundColor = "#748873";
-    text.style.innerText = "I am the orignal box ";
+    text.innerText = "I am the orignal box ";
 });
+
+// Now we are exploring the keypress event:- 
+let btn = document.querySelector("button");
+btn.style.marginLeft = "710px";
+btn.style.fontSize = "1.3em";
+btn.style.padding = "5px";
+
+btn.addEventListener("keydown", function () {
+    container.style.backgroundColor = "#3B060A";
+    container.style.color = "white";
+    text.innerText = "Keypress event has Activated";
+}); 
